91aae8f24da73844ab703c92931e1cf8376e62c8
007Alena <alena.7846@mail.ru>
2022-08-23 02:16:28 +0300

    lngapp

--- lang/src/App.js
+++ lang/src/App.js
@@ -1,15 +1,23 @@
-import './App.scss';
-import './App.scss';
-import Header from './components/header';
-import Main from './components/main';
-import Footer from './components/footer';
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Main />
-      <Footer />
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
