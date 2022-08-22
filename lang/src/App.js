0aa88dda1b37af5375763bdeeace399fea5676e2
007Alena <alena.7846@mail.ru>
2022-08-23 02:39:54 +0300

    lngapp

--- lang/src/App.js
+++ lang/src/App.js
@@ -1,12 +1,14 @@
 import './App.scss';
-import Header from './components/header.jsx';
+import Header from './components/header';
 import Main from './components/main';
+import Footer from './components/footer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Main />
+      <Footer />
     </div>
   );
 }
---
+++ lang/src/components/footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import './footer.scss';
+
+const Footer = () => {
+    return (
+        <footer className="footer">
+            <div>21/08/2022</div>
+        </footer>
+    );
+}
+
+export default Footer;
