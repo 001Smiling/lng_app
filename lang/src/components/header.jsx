4061564e5fb2226c99d9a4d877a50d39aa499894
007Alena <alena.7846@mail.ru>
2022-08-23 04:26:05 +0300

    lngapp

--- lang/src/components/header.jsx
+++ lang/src/components/header.jsx
@@ -1,12 +1,11 @@
 import React from "react";
-import './header.scss';
 
 const Header = () => {
     return (
         <header className="header">
             <nav className="nav">
                 <div className="nav__link">Избранное</div>
-                <div className="nav__link">В изучение</div>
+                <div className="nav__link">В изучении</div>
                 <div className="nav__link">Выученные</div>
             </nav>
         </header>
