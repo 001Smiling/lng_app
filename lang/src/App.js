6fcc74c784ff312195105d8bd7bead15b6301a59
007Alena <98030501+007Alena@users.noreply.github.com>
2023-01-02 01:09:04 +0300

    Update App.js
    /

--- lang/src/App.js
+++ lang/src/App.js
@@ -3,6 +3,7 @@ import Cardpage from './components/cardpage/cardpage';
 import Mainpage from './components/mainpage/mainpage';
 import Header from './components/header/header';
 import {
+  BrowserRouter,
   Routes,
   Route
 } from "react-router-dom";
@@ -11,10 +12,12 @@ function App() {
   return (
     <div className="App">
       <Header/>
+         <BrowserRouter>
             <Routes>
                 <Route exact path="/" element={<Mainpage />} />
                 <Route exact path="/cards" element={<Cardpage />} />
             </Routes>
+         </BrowserRouter>
     </div>
   );
 }
